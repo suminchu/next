85b567c9778cfba090ca2f0f8e847a108a377e20
suminchu <shelly7370@naver.com>
2024-08-05 13:31:58 +0900

    리액트 기초 2

--- 01리액트기초/src/App.jsx
+++ 01리액트기초/src/App.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+
 import PostsList from "./components/PostsList";
+import MainHeader from "./components/MainHeader";
 
 function App() {
-  return <main>
-        <PostsList/>
-  </main>;
+  const [modalIsVisible, setModalIsVisible] = useState(false); // 상태값이 true인경우 : 로딩시 모달창 띄워져 있음  / false인 경우: 로딩시 모달창 바로 안뜸
+
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function hideModalHandle() {
+    setModalIsVisible(false);
+}
+
+
+  return(
+  <>
+  <MainHeader onCreatePost={showModalHandler}/>
+    <main>
+        <PostsList 
+        isPosting={modalIsVisible} 
+        onStopPosting={hideModalHandle}
+        />
+    </main>
+  </>
+  )
 }
 
 export default App;
