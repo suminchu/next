07a83d02ea5173f042d8ea866a6845a557cba14f
suminchu <shelly7370@naver.com>
2024-08-05 20:20:27 +0900

    [#]2

--- 01리액트기초/src/components/PostsList.jsx
+++ 01리액트기초/src/components/PostsList.jsx
@@ -1,32 +1,24 @@
-import {useState} from 'react';
+
+import { useLoaderData } from 'react-router-dom';
 
 import Post from './post';
-import NewPost from './NewPost';
-import Modal from './Modal';
-
 import classes from './PostsList.module.css';
 
-function PostsList ({isPosting, onStopPosting}) {
-    const [posts, setPosts] = useState([]);
 
-    function addPostHandler (postData) {
-        setPosts((existingPosts) => [postData, ...existingPosts] );
-    }
+function PostsList ( ) {
+    const posts = useLoaderData();
+
 
     return(
     <>
-        {isPosting &&  (
-            <Modal onClose={onStopPosting}>
-                <NewPost onCancel={onStopPosting} onAddPost={addPostHandler}/>
-            </Modal> 
-        )}
-
-        {posts.length > 0 && (
+        { posts.length > 0 && (
         <ul className={classes.posts}>
-        {posts.map((post) => <Post key={post.body} author={post.author} body={post.body}/>)}
+        {posts.map((post) => (
+            <Post key={post.body} id={post.id} author={post.author} body={post.body}/>
+            ))}
         </ul>
         )}
-        {posts.length === 0 && ( <div style={{textAlign: 'center', color:'white'}}>
+        { posts.length === 0 && ( <div style={{textAlign: 'center', color:'white'}}>
             <h2>There are no posts yet.</h2>
             <p>Start adding some!</p>
             </div>
--- 01리액트기초/src/components/post.jsx
+++ 01리액트기초/src/components/post.jsx
@@ -1,12 +1,16 @@
+import { Link } from 'react-router-dom'
+
 import classes from'./post.module.css'
 
-function Post (props) {
+function Post ({id, author, body}) {
 
 
     return (
-        <li className={classes.post}>       
-        <p className={classes.author}>{props.author}</p>
-        <p className={classes.text}>{props.body}</p>
+        <li className={classes.post}>
+        <Link to={id}>     
+            <p className={classes.author}>{author}</p>
+            <p className={classes.text}>{body}</p>
+        </Link>  
     </li>
     );
 }
